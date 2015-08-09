740eaf2e95e3d85b457cd14bfd11af918b5cd946
Bradley Allen <bradley.allen84@gmail.com>
2015-08-09 13:24:09 -0400

    Update db_connect.php
    remove credentials from public view

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,9 @@
 <?php
-$host = "bushwald-pizza-1695243";
+$host = "****";
 $user = "bushwald";
-$pass = "";
-$db = "pizza_db";
-$port = 3306;
-
-//$conn = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+$pass = "****";
+$db = "****";
+$port = ****;
 
 //Create Connection
 $mysqli = new mysqli($host, $user, $pass, $db, $port);
